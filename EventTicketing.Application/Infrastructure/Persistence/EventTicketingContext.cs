985357574ffe6e6733e320044083aad18e558842
CarlosRamirez31 <cr2503150@gmail.com>
2023-04-24 22:49:53 -0400

    Adding Context

---
+++ EventTicketing.Application/Infrastructure/Persistence/EventTicketingContext.cs
@@ -0,0 +1,26 @@
+﻿using EventTicketing.Application.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventTicketing.Application.Infrastructure.Persistence
+{
+    public class EventTicketingContext : DbContext
+    {
+        public EventTicketingContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventPrice> EventPrices { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Zone> Zones { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(Application).Assembly);
+        }
+    }
+}
